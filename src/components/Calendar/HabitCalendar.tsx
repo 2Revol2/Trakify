5293243d1e803753b0b7cef1cf4715c6551ce562
Maksym Baluta <maksimbaluta02@gmail.com.com>
2025-01-03 17:03:10 +0300

    fix: bug fix

--- src/components/Calendar/HabitCalendar.tsx
+++ src/components/Calendar/HabitCalendar.tsx
@@ -1,12 +1,12 @@
 import Calendar from "react-calendar"
-import { useState } from "react"
-import style from './HabitCalendar.module.scss'
+
+
 import 'react-calendar/dist/Calendar.css'
 
 export const HabitCalendar = () => {
-  const [activeData, setActiveData] = useState<Date | undefined>(new Date)
+
   
   return (
-   <Calendar activeStartDate={activeData}/>
+   <Calendar />
   )
 }
