6b4d0c880259d6a0048817627e62dc3347109156
Maksym Baluta <maksimbaluta02@gmail.com.com>
2025-01-06 12:52:56 +0300

    feat: add profile page

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,16 +1,49 @@
 import style from "./Profile.module.scss";
 import { Title } from "../../components/Title/Title";
+import { HabitsType } from "../../Shared/Types";
+import { DaysStreak } from "../../components/DaysStreak/DaysStreak";
+import { useNavigate } from "react-router";
 import { Description } from "../../components/Description/Description";
 export const Profile = () => {
+  const data: HabitsType[] = JSON.parse(localStorage.getItem("habits") || "[]");
+  const navigate = useNavigate();
   return (
     <div className={style.wrapper}>
       <div>
-        <Title>Ваш профиль</Title>
+        <Title>Профиль</Title>
       </div>
       <div className={style.profile}>
-        <div className={style.blockInfo}>
-          <Description>Ваша почта</Description>
-          <p>test@test.com</p>
+        <div className={style.userInfo}>
+          <div className={style.blockInfo}>
+            <p>Ник</p>
+            <p>Пользователь</p>
+          </div>
+          <div className={style.blockInfo}>
+            <p>Почта</p>
+            <p>test@test.com</p>
+          </div>
+        </div>
+        
+        <div className={style.habitWrapper}>
+        <Description>Привычки</Description>
+          {data.map((habit, index) => {
+            return (
+              <div
+                className={style.habit}
+                key={index}
+                onClick={() => navigate(`/habit/${habit.name}`)}
+              >
+                <div>
+                  <p className={style.info}>Привычка</p>
+                  <Description>{habit?.name}</Description>
+                </div>
+                <div>
+                  <p className={style.info}>Дни выполнения</p>
+                  <DaysStreak>{habit.streak}</DaysStreak>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
